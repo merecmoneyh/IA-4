45343b71138d4aa73b9dc324fa5693bccfa4d3e2
Carlos Alberto <merecmoney5@gmail.com>
2018-09-23 18:00:38 -0500

    se modfica el archivo para aceptar una entrada de n variables

--- Dijkstra_Recursivo.py
+++ Dijkstra_Recursivo.py
@@ -1,11 +1,24 @@
+"""
+    Programa que evalua preposiciones compuestas mediante el algoritmo de Dijkstra Recursivo
+
+    Input:
+        La preposición dada por consola, por ejemplo:
+            ( ( p | q ) <=> ( ! r ) )
+
+        número de variables: para el ejemplo anterior son 3
+
+        nombres de las variables: para el ejemplo anterior son "p", "q" y "r"
+"""
+
 def idenElem(l):
-    """Consigue las operaciones que estan involucradas en la preposicion compuesta.
+    """
+    Consigue las preposiciones atómicas que estan involucradas en la preposicion compuesta.
     Estas son separadas por los paréntesis
 
     Args:
-        l: lista de los elementos de la operación
+        l: lista de los elementos de la preposición
     Returns:
-        Es una lista que contiene las operaciones las preposiciones atómicas con sus operandos
+        Es una lista que contiene las preposiciones atómicas
     """
     cnt=0
     aux,sal=[],[]
@@ -27,7 +40,7 @@ def evalua(fp, dic_elem, dic_op):
     Esta función obtiene el resultado de la preposición compuesta
 
     Va evaluando las preposiciones atómicas recursivamente y regresa su valor
-    hasta evalular la compuesta
+    hasta evalular la preposición compuesta completamente
 
     Args:
         fp: lista de los elementos de la operación
@@ -54,17 +67,53 @@ def evalua(fp, dic_elem, dic_op):
         val = eval(dic_op[elem[0][0]]+str(evalua(elem[1],dic_elem,dic_op)))
     return val
 
-fp='( ( p | q ) <=> ( ! r ) )'
-#fp='r'
-#dic_elem={'p':False,'q':True, 'r':True}
-dic_op={'|':' or ','&':' and ','!':'not '}
-lista=fp.split(' ')
+def create_dic_elem(n_elem, variable_names):
+    """
+    Funcion que nos regresa un diccionario con una combinación de valores
+    de las variables de la preposición compuesta dada por la cadena n_elem
 
-for p in [True,False]:
-    for q in [True,False]:
-        for r in [True,False]:
-            lista=fp.split(' ')
-            #print(str(p),str(q) ,str(r))
-            print(evalua(lista, {'p':p, 'q':q, 'r': r}, dic_op))
+    Por ejemplo si n_elem = "1000" para las variables [p,q,r,b]
+    entonces la combinación para 4 variables sería:
+    {'p': True, 'q': False, 'r': True, 'b': False}
+    Args:
+        n_elem: cadena que nos dice una combinación que se quiere de las variables
+        variable_names: lista que contiene los nombres de las variables
+        de la preposicón compuesta
+    Returns:
+        diccionario con una combinación de valores que fue dada
+    """
+    dic_elem = {}
+    count = 0
+    for i in list(n_elem):
+        if i != "0" :
+            dic_elem[variable_names[count]] = True
+        else:
+            dic_elem[variable_names[count]] = False
+        count+=1
+    print("para " + str(dic_elem) + "\n")
+    return dic_elem
 
-#print(evalua(lista,dic_elem,dic_op))
+def main():
+    """
+        Funcion que pide las entradas del programa y nos arroja los resultados
+    """
+    dic_op={'|':' or ','&':' and ','!':'not '}
+
+    print("dime la preposición compuesta")
+    fp = input()
+    print("dime el número de variables")
+    n_variables = int(input())
+
+    variable_names = []
+    for i in range(n_variables):
+        print("dime el nombre de la variable " + str(i+1))
+        variable_names.append(input())
+
+    print("resultado")
+
+    for i in range(2**n_variables):
+        n_elem = bin(i)[2:].zfill(n_variables)
+        lista=fp.split(' ')
+        print(evalua(lista, create_dic_elem(n_elem, variable_names), dic_op))
+
+main()
