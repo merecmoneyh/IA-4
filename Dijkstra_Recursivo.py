238fc458c26f92fc7cd67fd6f86740c9ba58d681
Carlos Alberto <merecmoney5@gmail.com>
2018-09-23 22:05:33 -0500

    agrega modificaciones para que las preposiciones puedan tener  variables tengan valores constantes las cuales son TRUE o FALSE, y  se permite tener variables no constantes y constantes

--- Dijkstra_Recursivo.py
+++ Dijkstra_Recursivo.py
@@ -70,7 +70,7 @@ def evalua(fp, dic_elem, dic_op):
         val = eval(dic_op[elem[0][0]]+str(evalua(elem[1],dic_elem,dic_op)))
     return val
 
-def create_dic_elem(n_elem, variable_names):
+def create_dic_elem(n_elem, variable_names, dic_elem):
     """
     Funcion que nos regresa un diccionario con una combinación de valores
     de las variables de la preposición compuesta dada por la cadena n_elem
@@ -79,13 +79,13 @@ def create_dic_elem(n_elem, variable_names):
     entonces la combinación para 4 variables sería:
     {'p': True, 'q': False, 'r': True, 'b': False}
     Args:
+        dic_elem: pasamos la referencia de un diccionario para agregarle valores
         n_elem: cadena que nos dice una combinación que se quiere de las variables
         variable_names: lista que contiene los nombres de las variables
         de la preposicón compuesta
     Returns:
         diccionario con una combinación de valores que fue dada
     """
-    dic_elem = {}
     count = 0
     for i in list(n_elem):
         if i != "0" :
@@ -107,16 +107,46 @@ def main():
     print("dime el número de variables")
     n_variables = int(input())
 
+    n_variables_1 = 0
+    n_variable_true = False
+    n_variable_false = False
     variable_names = []
     for i in range(n_variables):
         print("dime el nombre de la variable " + str(i+1))
         variable_names.append(input())
+        #checar si true y false son variables
+        if variable_names[i].lower() == 'true':
+            n_variable_true = True
+            variable_names[i] = variable_names[i].lower()
+        elif variable_names[i].lower() == 'false':
+            n_variable_false = True
+            variable_names[i] = variable_names[i].lower()
+        else:
+            #contar el numero de variables que no son TRUE o FALSE
+            n_variables_1 += 1
+
+    dic_elementos = {}
+    #si ambas son variables, agregarlas al diccionario
+    #si no agregarlas individualmente
+    if n_variable_false == True and n_variable_true == True:
+        dic_elementos['true'] = True
+        dic_elementos['false'] = False
+    elif n_variable_false == True:
+        dic_elementos['false'] = False
+    elif n_variable_true == True:
+        dic_elementos['true'] = True
 
     print("resultado")
 
-    for i in range(2**n_variables):
-        n_elem = bin(i)[2:].zfill(n_variables)
+    #si n_variables_1 es igual a cero, significa que la preposición tiene solo variables TRUE y FALSE
+    if (n_variables_1 == 0):
+        print("para " + str(dic_elementos) + "\n")
+        print(evalua(fp.split(' '),dic_elementos, dic_op))
+        return
+
+    for i in range(2**n_variables_1):
+        n_elem = bin(i)[2:].zfill(n_variables_1)
         lista=fp.split(' ')
-        print(evalua(lista, create_dic_elem(n_elem, variable_names), dic_op))
+        print(evalua(lista, create_dic_elem(n_elem, variable_names, dic_elementos), dic_op))
 
 main()
